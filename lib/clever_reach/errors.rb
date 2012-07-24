283ca2e3f1f1d12187c000731a01c6dde60aa1db
Stephan Schubert <stephan@frozencherry.de>
2012-07-24 14:50:20 +0200

    Add custom error classes.

--- lib/clever_reach/errors.rb
+++ lib/clever_reach/errors.rb
@@ -1,5 +1,37 @@
 module CleverReach
   module Errors
+    # http://support.cleverreach.de/entries/20674817-fehlercode-api
+    ErrorMap = {
+      1   => 'invalid API Key',
+      5   => 'need session before usage',
+      10  => 'permission denied',
+      20  => 'data not found',
+      30  => 'email invalid',
+      40  => 'email blacklisted',
+      50  => 'duplicate data',
+      60  => 'subscriber already active',
+      70  => 'inserting order failed',
+      80  => 'batch to big (max 50)',
+      90  => 'subscriber already inactive',
+      100 => 'given email is to short',
+      110	=> 'no forms available',
+      120	=> 'error saving filter'
+    }
+
     class BaseError < StandardError; end
+    class InvalidAPIKey < BaseError; end
+    class NeedSessionBeforeUsage < BaseError; end
+    class PermissionDenied < BaseError; end
+    class DataNotFound < BaseError; end
+    class EmailInvalid < BaseError; end
+    class EmailBlacklisted < BaseError; end
+    class DuplicateData < BaseError; end
+    class SubscriberAlreadyActive < BaseError; end
+    class InsertingOrderFailed < BaseError; end
+    class BatchTooBig < BaseError; end
+    class SubscriberAlreadyInactive < BaseError; end
+    class GivenEmailIsTooShort < BaseError; end
+    class NoFormsAvailable < BaseError; end
+    class ErrorSavingFilter < BaseError; end
   end
 end
--- spec/clever_reach/errors_spec.rb
+++ spec/clever_reach/errors_spec.rb
@@ -12,4 +12,66 @@ describe CleverReach::Errors do
 
   end
 
+  describe "Custom errors" do
+
+    let(:error_class) { CleverReach::Errors::BaseError }
+
+    it "should define an error for 'invalid API Key'" do
+      CleverReach::Errors::InvalidAPIKey.new.should be_kind_of(error_class)
+    end
+
+    it "should define an error for 'need session before usage'" do
+      CleverReach::Errors::NeedSessionBeforeUsage.new.should be_kind_of(error_class)
+    end
+
+    it "should define an error for 'permission denied'" do
+      CleverReach::Errors::PermissionDenied.new.should be_kind_of(error_class)
+    end
+
+    it "should define an error for 'data not found'" do
+      CleverReach::Errors::DataNotFound.new.should be_kind_of(error_class)
+    end
+
+    it "should define an error for 'email invalid'" do
+      CleverReach::Errors::EmailInvalid.new.should be_kind_of(error_class)
+    end
+
+    it "should define an error for 'email blacklisted'" do
+      CleverReach::Errors::EmailBlacklisted.new.should be_kind_of(error_class)
+    end
+
+    it "should define an error for 'duplicate data'" do
+      CleverReach::Errors::DuplicateData.new.should be_kind_of(error_class)
+    end
+
+    it "should define an error for 'subscriber already active'" do
+      CleverReach::Errors::SubscriberAlreadyActive.new.should be_kind_of(error_class)
+    end
+
+    it "should define an error for 'inserting order failed'" do
+      CleverReach::Errors::InsertingOrderFailed.new.should be_kind_of(error_class)
+    end
+
+    it "should define an error for 'batch to big (max 50)'" do
+      CleverReach::Errors::BatchTooBig.new.should be_kind_of(error_class)
+    end
+
+    it "should define an error for 'subscriber already inactive'" do
+      CleverReach::Errors::SubscriberAlreadyInactive.new.should be_kind_of(error_class)
+    end
+
+    it "should define an error for 'given email is too short'" do
+      CleverReach::Errors::GivenEmailIsTooShort.new.should be_kind_of(error_class)
+    end
+
+    it "should define an error for 'no forms available'" do
+      CleverReach::Errors::NoFormsAvailable.new.should be_kind_of(error_class)
+    end
+
+    it "should define an error for 'error saving filter'" do
+      CleverReach::Errors::ErrorSavingFilter.new.should be_kind_of(error_class)
+    end
+
+  end
+
 end
