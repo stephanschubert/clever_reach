c2f43115228a2e8ee479909a071dcb4632ba852a
Stephan Schubert <stephan@frozencherry.de>
2012-07-27 14:11:53 +0200

    Add custom error classes mapping to CleverReach's error codes.

--- lib/clever_reach/errors.rb
+++ lib/clever_reach/errors.rb
@@ -1,37 +1,113 @@
 module CleverReach
   module Errors
     # http://support.cleverreach.de/entries/20674817-fehlercode-api
+
+    class BaseError < StandardError
+    end
+
+    class InvalidAPIKey < BaseError;
+      def initialize
+        super('invalid API key')
+      end
+    end
+
+    class NeedSessionBeforeUsage < BaseError
+      def initialize
+        super('need session before usage')
+      end
+    end
+
+    class PermissionDenied < BaseError
+      def initialize
+        super('permission denied')
+      end
+    end
+
+    class DataNotFound < BaseError
+      def initialize
+        super('data not found')
+      end
+    end
+
+    class EmailInvalid < BaseError
+      def initialize
+        super('email invalid')
+      end
+    end
+
+    class EmailBlacklisted < BaseError
+      def initialize
+        super('email blacklisted')
+      end
+    end
+
+    class DuplicateData < BaseError
+      def initialize
+        super('duplicate data')
+      end
+    end
+
+    class SubscriberAlreadyActive < BaseError
+      def initialize
+        super('subscriber already active')
+      end
+    end
+
+    class InsertingOrderFailed < BaseError
+      def initialize
+        super('inserting order failed')
+      end
+    end
+
+    class BatchTooBig < BaseError
+      def initialize
+        super('batch too big (max 50)')
+      end
+    end
+
+    class SubscriberAlreadyInactive < BaseError
+      def initialize
+        super('subscriber already inactive')
+      end
+    end
+
+    class GivenEmailIsTooShort < BaseError
+      def initialize
+        super('given email is too short')
+      end
+    end
+
+    class NoFormsAvailable < BaseError
+      def initialize
+        super('no forms available')
+      end
+    end
+
+    class ErrorSavingFilter < BaseError
+      def initialize
+        super('error saving filter')
+      end
+    end
+
     ErrorMap = {
-      1   => 'invalid API Key',
-      5   => 'need session before usage',
-      10  => 'permission denied',
-      20  => 'data not found',
-      30  => 'email invalid',
-      40  => 'email blacklisted',
-      50  => 'duplicate data',
-      60  => 'subscriber already active',
-      70  => 'inserting order failed',
-      80  => 'batch to big (max 50)',
-      90  => 'subscriber already inactive',
-      100 => 'given email is to short',
-      110	=> 'no forms available',
-      120	=> 'error saving filter'
+      1   => InvalidAPIKey,
+      5   => NeedSessionBeforeUsage,
+      10  => PermissionDenied,
+      20  => DataNotFound,
+      30  => EmailInvalid,
+      40  => EmailBlacklisted,
+      50  => DuplicateData,
+      60  => SubscriberAlreadyActive,
+      70  => InsertingOrderFailed,
+      80  => BatchTooBig,
+      90  => SubscriberAlreadyInactive,
+      100 => GivenEmailIsTooShort,
+      110	=> NoFormsAvailable,
+      120	=> ErrorSavingFilter,
     }
 
-    class BaseError < StandardError; end
-    class InvalidAPIKey < BaseError; end
-    class NeedSessionBeforeUsage < BaseError; end
-    class PermissionDenied < BaseError; end
-    class DataNotFound < BaseError; end
-    class EmailInvalid < BaseError; end
-    class EmailBlacklisted < BaseError; end
-    class DuplicateData < BaseError; end
-    class SubscriberAlreadyActive < BaseError; end
-    class InsertingOrderFailed < BaseError; end
-    class BatchTooBig < BaseError; end
-    class SubscriberAlreadyInactive < BaseError; end
-    class GivenEmailIsTooShort < BaseError; end
-    class NoFormsAvailable < BaseError; end
-    class ErrorSavingFilter < BaseError; end
+    def self.lookup(code)
+      ErrorMap[code]
+    end
   end
 end
--- spec/clever_reach/errors_spec.rb
+++ spec/clever_reach/errors_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe CleverReach::Errors do
 
-  describe CleverReach::Errors::BaseError do
+  describe CleverReach::Errors::BaseError do # -------------
 
     let(:subject) { CleverReach::Errors::BaseError.new }
 
@@ -12,66 +12,106 @@ describe CleverReach::Errors do
 
   end
 
-  describe "Custom errors" do
+  describe "Custom errors" do # ----------------------------
 
-    let(:error_class) { CleverReach::Errors::BaseError }
+    let(:error_scope) { CleverReach::Errors }
+    let(:error_class) { error_scope::BaseError }
 
     it "should define an error for 'invalid API Key'" do
-      CleverReach::Errors::InvalidAPIKey.new.should be_kind_of(error_class)
+      error = error_scope::InvalidAPIKey.new
+      error.should be_kind_of(error_class)
+      error.message.should == 'invalid API key'
     end
 
     it "should define an error for 'need session before usage'" do
-      CleverReach::Errors::NeedSessionBeforeUsage.new.should be_kind_of(error_class)
+      error = error_scope::NeedSessionBeforeUsage.new
+      error.should be_kind_of(error_class)
+      error.message.should == 'need session before usage'
     end
 
     it "should define an error for 'permission denied'" do
-      CleverReach::Errors::PermissionDenied.new.should be_kind_of(error_class)
+      error = error_scope::PermissionDenied.new
+      error.should be_kind_of(error_class)
+      error.message.should == 'permission denied'
     end
 
     it "should define an error for 'data not found'" do
-      CleverReach::Errors::DataNotFound.new.should be_kind_of(error_class)
+      error = error_scope::DataNotFound.new
+      error.should be_kind_of(error_class)
+      error.message.should == 'data not found'
     end
 
     it "should define an error for 'email invalid'" do
-      CleverReach::Errors::EmailInvalid.new.should be_kind_of(error_class)
+      error = error_scope::EmailInvalid.new
+      error.should be_kind_of(error_class)
+      error.message.should == 'email invalid'
     end
 
     it "should define an error for 'email blacklisted'" do
-      CleverReach::Errors::EmailBlacklisted.new.should be_kind_of(error_class)
+      error = error_scope::EmailBlacklisted.new
+      error.should be_kind_of(error_class)
+      error.message.should == 'email blacklisted'
     end
 
     it "should define an error for 'duplicate data'" do
-      CleverReach::Errors::DuplicateData.new.should be_kind_of(error_class)
+      error = error_scope::DuplicateData.new
+      error.should be_kind_of(error_class)
+      error.message.should == 'duplicate data'
     end
 
     it "should define an error for 'subscriber already active'" do
-      CleverReach::Errors::SubscriberAlreadyActive.new.should be_kind_of(error_class)
+      error = error_scope::SubscriberAlreadyActive.new
+      error.should be_kind_of(error_class)
+      error.message.should == 'subscriber already active'
     end
 
     it "should define an error for 'inserting order failed'" do
-      CleverReach::Errors::InsertingOrderFailed.new.should be_kind_of(error_class)
+      error = error_scope::InsertingOrderFailed.new
+      error.should be_kind_of(error_class)
+      error.message.should == 'inserting order failed'
     end
 
     it "should define an error for 'batch to big (max 50)'" do
-      CleverReach::Errors::BatchTooBig.new.should be_kind_of(error_class)
+      error = error_scope::BatchTooBig.new
+      error.should be_kind_of(error_class)
+      error.message.should == 'batch too big (max 50)'
     end
 
     it "should define an error for 'subscriber already inactive'" do
-      CleverReach::Errors::SubscriberAlreadyInactive.new.should be_kind_of(error_class)
+      error = error_scope::SubscriberAlreadyInactive.new
+      error.should be_kind_of(error_class)
+      error.message.should == 'subscriber already inactive'
     end
 
     it "should define an error for 'given email is too short'" do
-      CleverReach::Errors::GivenEmailIsTooShort.new.should be_kind_of(error_class)
+      error = error_scope::GivenEmailIsTooShort.new
+      error.should be_kind_of(error_class)
+      error.message.should == 'given email is too short'
     end
 
     it "should define an error for 'no forms available'" do
-      CleverReach::Errors::NoFormsAvailable.new.should be_kind_of(error_class)
+      error = error_scope::NoFormsAvailable.new
+      error.should be_kind_of(error_class)
+      error.message.should == 'no forms available'
     end
 
     it "should define an error for 'error saving filter'" do
-      CleverReach::Errors::ErrorSavingFilter.new.should be_kind_of(error_class)
+      error = error_scope::ErrorSavingFilter.new
+      error.should be_kind_of(error_class)
+      error.message.should == 'error saving filter'
     end
 
   end
+
+  describe ".lookup" do # ----------------------------------
+
+    CleverReach::Errors::ErrorMap.each do |code, klass|
+      it "should map error code #{code} to an error class '#{klass.name}'" do
+        subject.lookup(code).should == klass
+      end
+    end
+
+  end
+
 
 end
